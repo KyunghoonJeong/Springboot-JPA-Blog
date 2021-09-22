940422dc6c78c16d8e9dba225aa389f61dea360a
KyunghoonJeong <90249583+KyunghoonJeong@users.noreply.github.com>
2021-09-22 18:29:58 +0900

    fk설정과 조인관계 설정 완료(웹페이지 형태를 미리 생각해서 구성)

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,18 +1,22 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -45,11 +49,12 @@ public class Board {
 	@JoinColumn(name="userid") //필드값은 유저아이디로 만들어지고 연관관계는 매니투원으로 만들어진다. 
 	private User user; //조인컬럼을 통해서 원래 디비는 객체 인식이 안되나 이것을 통해 객체 인식이 가능해진다. 중간 다리 역할을 해줌
 	
-	
-	
+	@OneToMany(mappedBy="board", fetch= FetchType.EAGER) //하나의 게시글은 여러 개의 답변을 지닐 수 있음 //맵 바이 => 연관관계의 주인이 아니다 -> 난 fk가 아니니 db에 컬럼을 만들지 마세요 라는 의미이다.
+	//	@JoinColumn(name="replyid")  //replyid라는 fk는 필요없기 때문에 이건 안써줘도 된다. //디폴트 패치값이 레이지긴 한데 이것이 의미하는 것은 필요할 때 가져온다는 것이고 Eager는 즉시 가져오는 것인데 댓글창이 바로 보이게끔 설정한다면 EAGER전략을 써야한다.
+	private List<Reply> reply; //보드에서 값을 얻기 위해서만 필요한 것이다 => 맵바이 보드를 통해서 fk가 아님을 알림
 	@CreationTimestamp
 	private Timestamp createDate; //현재 시간 ! 자동으로 들어가게끔
 	
-	
+	//이해가 잘 안가도 몇번 해보면 알 것이다.
 
 }
